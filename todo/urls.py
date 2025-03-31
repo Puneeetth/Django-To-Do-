c40b9b59296e26c0a17344d4f85faee45c9a4eb6
Pueeeth <punithreddy417@gmail.com>
2025-03-31 22:23:23 +0530

    urls,models

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import TaskListView, TaskCreateView, TaskDeleteView, TaskUpdateView
+
+urlpatterns = [
+    path('',TaskListView.as_view(),name='task_list'),
+    path('task/new/',TaskCreateView.as_view(),name='task_create'),
+    path('task/<int:pk>/edit',TaskUpdateView.as_view(),name='task_edit'),
+    path('task/<int:pk>/delete',TaskDeleteView.as_view(),name='task_delete'),
+    
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Task
+from .forms import TaskForm
+from django.views.generic import ListView, CreateView, UpdateView ,DeleteView
 
-# Create your views here.
+class TaskListView(ListView):
+    model = Task
+    template_name = 'todo/task_list.html'
+    context_object_name = 'tasks'
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'todo/task_form.html'
+    success_url ='/'
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'todo/task_form.html'
+    success_url = '/'
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'todo/task_confirm_delete.html'
+    success_url = '/'
+    
+    
